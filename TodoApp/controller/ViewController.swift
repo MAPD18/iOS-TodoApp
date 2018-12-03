0f1a4668b0911a7b2628de9572f40b92f6b2f14d
Rodrigo Silva <digovs@gmail.com>
2018-12-02 23:42:44 -0500

    Creating/Updating Tasks on Firestore; Deleting Tasks from Firestore

--- TodoApp/controller/ViewController.swift
+++ TodoApp/controller/ViewController.swift
@@ -19,17 +19,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let sectionHeaderHeight: CGFloat = 25
     var db : Firestore?
-    
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate 
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var data = [TableSection: [TaskModel]]()
     
     func saveTask(selectedTask: SelectedTask) {
-        let updatedTask = data[TableSection(rawValue: selectedTask.section!)!]?[selectedTask.index!]
-        updatedTask?.name = (selectedTask.task?.name)!
-        updatedTask?.details = (selectedTask.task?.details)!
-        
-        tableView.reloadSections([selectedTask.section!], with: .automatic)
+        db?.collection(AppConstants.FIREBASE_BASE_PATH).document((selectedTask.task?.documentId)!).setData([
+            AppConstants.TASK_NAME_FIELD : selectedTask.task?.name as Any,
+            AppConstants.TASK_DETAILS_FIELD : selectedTask.task?.details as Any,
+            AppConstants.TASK_CHECKED_FIELD: selectedTask.task?.checked as Any
+        ]) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+                let updatedTask = self.data[TableSection(rawValue: selectedTask.section!)!]?[selectedTask.index!]
+                updatedTask?.name = (selectedTask.task?.name)!
+                updatedTask?.details = (selectedTask.task?.details)!
+                
+                self.tableView.reloadSections([selectedTask.section!], with: .automatic)
+            }
+        }
+    }
+    
+    func saveTask(name: String, details: String) {
+        var ref: DocumentReference? = nil
+        ref = db?.collection(AppConstants.FIREBASE_BASE_PATH).addDocument(data: [
+            AppConstants.TASK_NAME_FIELD : name,
+            AppConstants.TASK_DETAILS_FIELD : details,
+            AppConstants.TASK_CHECKED_FIELD: false
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+                self.data[.todo]?.append(TaskModel(documentId: ref!.documentID, name: name, details: details))
+                self.tableView.reloadSections([TableSection.todo.rawValue], with: .automatic)
+            }
+        }
     }
     
     func onTaskSelected(section: Int, index: Int) {
@@ -50,11 +76,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         
         tableView.reloadSections([TableSection.todo.rawValue, TableSection.done.rawValue], with: .automatic)
-    }
-    
-    func saveTask(name: String, details: String) {
-        data[.todo]?.append(TaskModel(documentId: "1eac431s", name: name, details: details))
-        tableView.reloadSections([TableSection.todo.rawValue], with: .automatic)
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -134,11 +155,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
             if let tableSection = TableSection(rawValue: indexPath.section) {
-                self.data[tableSection]?.remove(at: indexPath.row)
-                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                let taskDocumentId = self.data[tableSection]?[indexPath.row].documentId
+                self.db?.collection(AppConstants.FIREBASE_BASE_PATH).document(taskDocumentId!).delete() { err in
+                    if let err = err {
+                        print("Error removing document: \(err)")
+                    } else {
+                        print("Document successfully removed!")
+                        self.data[tableSection]?.remove(at: indexPath.row)
+                        self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                    }
+                }
             }
             completion(true)
         }
+        
         deleteAction.backgroundColor = .red
         return deleteAction
     }
@@ -167,28 +197,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
         
-//        var ref: DocumentReference? = nil
-//        ref = db.collection("users/9je10kTQLrZCXNEoDlBy/tasks").addDocument(data: [
-//            "name": "New Task",
-//            "details": "New Task Details",
-//            "checked": false
-//        ]) { err in
-//            if let err = err {
-//                print("Error adding document: \(err)")
-//            } else {
-//                print("Document added with ID: \(ref!.documentID)")
-//            }
-//        }
-//
         
         
-//        db.collection("users/9je10kTQLrZCXNEoDlBy/tasks").document("QEGDXwZNzURzvOxdIkVV").delete() { err in
-//            if let err = err {
-//                print("Error removing document: \(err)")
-//            } else {
-//                print("Document successfully removed!")
-//            }
-//        }
+        
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
